4e279c3daab2122a8f5ec43196453b79e33ab3db
Amiqra <72076756+Amiqra@users.noreply.github.com>
2020-10-30 22:07:03 +0000

    maliye ekleme

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,7 @@ public class hello {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello little sunshine!");
+        System.out.println("Hello little sunshine!");  // ekleme
 
 
         System.out.println("second hand");
